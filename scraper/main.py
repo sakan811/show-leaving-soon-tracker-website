7d52c661b1517308a4f2da83b629f8a48d1eca34
Sakan Nirattisaykul <94357278+sakan811@users.noreply.github.com>
2025-01-15 14:02:58 +0700

    Refactor main.py to use StreamingAvailabilityScraper for fetching leaving titles. Removed hardcoded URL and updated the method to retrieve and save leaving titles for January 2025, enhancing the scraper's functionality and flexibility.

--- scraper/main.py
+++ scraper/main.py
@@ -1,18 +1,16 @@
-from netflix_scraper import NetflixScraper
+from netflix_scraper import StreamingAvailabilityScraper
 import sys
 import os
 
 def main():
-    url = "https://www.netflix.com/tudum/articles/whats-leaving-netflix"
-    
     # Get the absolute path to the frontend data directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.dirname(current_dir)
     data_path = os.path.join(project_root, 'frontend', 'public', 'data', 'leaving_titles_jan_2025.json')
     
     try:
-        with NetflixScraper(headless=True) as scraper:
-            leaving_titles = scraper.get_leaving_dates_and_titles(url)
+        with StreamingAvailabilityScraper() as scraper:
+            leaving_titles = scraper.get_leaving_titles()
             scraper.save_leaving_titles(leaving_titles, data_path)
                 
     except Exception as e:
